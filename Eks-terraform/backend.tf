53ed1bc8ffb1f392986524e4f6291c48a7085e71
ap786771 <154984891+ap786771@users.noreply.github.com>
2025-08-16 19:07:53 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777-ap-south-1"
+    bucket = "ajay-mrcloudbook777-mumbai"
     key    = "env:/terraform.tfstate"
     region = "ap-south-1"
   }
