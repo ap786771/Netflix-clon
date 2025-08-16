ff35b2de469b710cf3d15b81a0deb324be683b55
ap786771 <154984891+ap786771@users.noreply.github.com>
2025-08-16 19:06:06 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "ajay-mrcloudbook777"
-    key            = "env:/terraform.tfstate"
-    region         = "us-east-1"   # <-- must match real region
-    dynamodb_table = "terraform-lock" # only if using locking
-    encrypt        = true
+    bucket = "ajay-mrcloudbook777-ap-south-1"
+    key    = "env:/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
